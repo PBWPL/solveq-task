26142aaca52e97ae0f35ee220d3596e3124e2e30
Piotr Bęc <becpiotr@gmail.com>
2022-11-28 21:32:17 +0100

    updated README.md and deploy-prod.sh

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,12 +4,16 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-echo "[${RED}1${NC}/5] ${GREEN}list of running containers${NC}"
+echo "[${RED}1${NC}/7] ${GREEN}building app${NC}"
+npm run build
+echo "[${RED}2${NC}/7] ${GREEN}list of running containers${NC}"
 docker ps
-echo "[${RED}2${NC}/5] ${GREEN}stopping running containers${NC}"
+echo "[${RED}3${NC}/7] ${GREEN}stopping running containers${NC}"
 docker-compose stop
-echo "[${RED}3${NC}/5] ${GREEN}removing running containers${NC}"
+echo "[${RED}4${NC}/7] ${GREEN}removing running containers${NC}"
 docker-compose rm -f
-echo "[${RED}4${NC}/5] ${GREEN}starting containers${NC}"
+echo "[${RED}5${NC}/7] ${GREEN}starting containers${NC}"
 docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up -d --build
-echo "[${GREEN}5/5${NC}] ${GREEN}deployment complete${NC}"
+echo "[${RED}6${NC}/7] ${GREEN}importing database${NC}"
+docker-compose exec -T db mysql -uroot -ppassword task <db-data.sql
+echo "[${GREEN}7/7${NC}] ${GREEN}deployment complete${NC}"
